19638a8bd9ad3e4e1e9202eb96dc1e8c130f09c9
Nat Karmios <nat@karmios.com>
2024-08-12 04:06:35 +0100

    Canonicalise config path

--- src/config.rs
+++ src/config.rs
@@ -15,7 +15,9 @@ pub struct Config {
 
 impl Config {
     pub fn load(path: &Path) -> Result<Self> {
-        let file = File::open(path).with_context(|| format!("Couldn't open {}", path.display()))?;
+        let path = std::fs::canonicalize(path)?;
+        let file =
+            File::open(&path).with_context(|| format!("Couldn't open {}", path.display()))?;
         let reader = BufReader::new(file);
         serde_yaml::from_reader(reader)
             .with_context(|| format!("Failed to parse {}", path.display()))
